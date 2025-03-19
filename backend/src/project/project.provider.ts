8fdb6b3a4e49e7869f2cdf5a7b55c27b6f760c06
Luan <devluanpaiva@gmail.com>
2025-03-19 14:32:43 -0300

    feat: create project provider

---
+++ backend/src/project/project.provider.ts
@@ -0,0 +1,17 @@
+import { Project } from '@core';
+import { Injectable } from '@nestjs/common';
+import { PrismaProvider } from 'src/database/prisma.provider';
+
+@Injectable()
+export class ProjectProvider {
+    constructor(private readonly prisma: PrismaProvider) { }
+    async getAllProjects(): Promise<Project[]> {
+        return await this.prisma.project.findMany() as any;
+    }
+    async getProjectById(id: number): Promise<Project | null> {
+        return await this.prisma.project.findUnique({
+            where: { id },
+            include: { technologies: true }
+        }) as any;
+    }
+}
