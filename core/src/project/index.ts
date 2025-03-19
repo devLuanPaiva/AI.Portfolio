7c1a96e9f99195a06b1203a6ada8bebf74aa0c60
Luan <devluanpaiva@gmail.com>
2025-03-19 11:44:08 -0300

    feat: create project model

---
+++ core/src/project/index.ts
@@ -0,0 +1,5 @@
+import { Level } from './models/enums/Level.enum'
+import { Type } from './models/enums/Type.enum'
+import Project from './models/interfaces/Project.interface'
+export { Level, Type }
+export type { Project }
---
+++ core/src/project/models/enums/Level.enum.ts
@@ -0,0 +1,6 @@
+export enum Level {
+    BEGINNER = 1,
+    INTERMEDIARY = 2,
+    AVANCED = 3,
+    EXPERT = 4
+}
