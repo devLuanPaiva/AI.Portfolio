13fc7fbee7e54587f1651664c8e8b53cab185c9d
Luan <devluanpaiva@gmail.com>
2025-03-20 13:43:01 -0300

    test: add test to return highlighted technologies

--- backend/src/technology/technology.provider.spec.ts
+++ backend/src/technology/technology.provider.spec.ts
@@ -1,4 +1,4 @@
-import { PrismaProvider } from "src/database/prisma.provider";
+import { PrismaProvider } from "../database/prisma.provider";
 import { TechnologyProvider } from "./technology.provider";
 import { Test, TestingModule } from "@nestjs/testing";
 import { Technology } from "@core";
@@ -41,5 +41,24 @@ describe('TechnologyProvider', () => {
         const technologies = await technologyProvider.getAllTechnologies();
         expect(technologies).toEqual(mockTechnologies);
         expect(prismaProvider.technology.findMany).toHaveBeenCalledTimes(1);
+    });
+    it('should return highlighted technologies', async () => {
+        const mockHighlightedTechnologies: Technology[] = [
+            {
+                id: 2,
+                name: 'Tech 2',
+                description: 'Description 2',
+                image: 'img2.jpg',
+                highlighted: true,
+            },
+        ];
+
+        jest.spyOn(prismaProvider.technology, 'findMany').mockResolvedValue(mockHighlightedTechnologies);
+
+        const technologies = await technologyProvider.gethighlighted();
+        expect(technologies).toEqual(mockHighlightedTechnologies);
+        expect(prismaProvider.technology.findMany).toHaveBeenCalledWith({
+            where: { highlighted: true },
+        });
     });
 })
