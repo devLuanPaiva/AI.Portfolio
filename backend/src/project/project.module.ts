e573f797134ff06677c9c4272ffa1efe761b91cf
Luan <devluanpaiva@gmail.com>
2025-03-20 14:34:42 -0300

    fix: adjust import path

--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProjectController } from './project.controller';
 import { ProjectProvider } from './project.provider';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   providers: [ProjectProvider],
