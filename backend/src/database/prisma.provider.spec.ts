339d703d7e2c2c88031b314b8ecc646051347844
Luan <devluanpaiva@gmail.com>
2025-03-20 13:19:20 -0300

    test: add test to call  $connect on module init

--- backend/src/database/prisma.provider.spec.ts
+++ backend/src/database/prisma.provider.spec.ts
@@ -15,4 +15,9 @@ describe('PrismaProvider', () => {
   it('should be defined', () => {
     expect(provider).toBeDefined();
   });
+  it('should call $connect on module init', async () => {
+    const connectSpy = jest.spyOn(provider, '$connect').mockResolvedValue(undefined);
+    await provider.onModuleInit();
+    expect(connectSpy).toHaveBeenCalledTimes(1);
+  });
 });
