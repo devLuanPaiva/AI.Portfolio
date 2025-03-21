d7900655af8d24205c8ff7a628a4b56ac388fe68
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-21 16:38:54 -0300

    feat: use header component in the layout

--- frontend/src/app/(pages)/layout.tsx
+++ frontend/src/app/(pages)/layout.tsx
@@ -1,8 +1,10 @@
 import ChatButton from "@/components/chat/ChatButton";
+import Header from "@/components/template/Header";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main>
+            <Header/>
             {children}
             <ChatButton />
         </main>
