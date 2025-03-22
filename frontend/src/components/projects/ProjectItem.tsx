98bc07890e4567afe644b496ab0e56a2710a0fec
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-22 12:22:17 -0300

    feat: create projects components

---
+++ frontend/src/components/projects/ProjectItem.tsx
@@ -0,0 +1,18 @@
+import { ProjectProps } from "@/data/interfaces";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ProjectItem(props: Readonly<ProjectProps>) {
+    return (
+        <Link href={`/project/${props.project.id}`} >
+            <article className="relative rounded-2xl overflow-hidden border border-zinc-800 min-w-64 min-h-64">
+                <Image
+                    src={props.project.images[0]}
+                    alt={props.project.name}
+                    fill
+                    objectFit="cover"
+                />
+            </article>
+        </Link>
+    )
+}
