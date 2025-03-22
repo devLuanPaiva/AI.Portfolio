328ba6539b053d0698709aeefcf2a044ebc4eb88
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-22 12:22:49 -0300

    feat: use projects components

--- frontend/src/app/(pages)/page.tsx
+++ frontend/src/app/(pages)/page.tsx
@@ -1,18 +1,25 @@
 'use client'
 import Curriculum from "@/components/document/Curriculum";
 import Landing from "@/components/Landing/Landing";
+import ProjectsList from "@/components/projects/ProjectsList";
 import Container from "@/components/template/Container";
+import { useProjects } from "@/data/hooks/useProjects";
 import { useTechnologies } from "@/data/hooks/useTechnologies";
 import { Suspense } from "react";
 
 export default function Home() {
   const { technologies, highlightedTechnologies, loading } = useTechnologies()
+  const { gameProjects, highlightedProjects, mobileProjects, webProjects } = useProjects()
   if (loading) return
 
   return (
     <Suspense fallback={<p>carregando...</p>} >
       <Landing technologies={highlightedTechnologies} />
-      <Container classStyle="py-20">
+      <Container classStyle="py-20 flex flex-col gap-7">
+        <ProjectsList title="Destaques" projects={highlightedProjects} />
+        <ProjectsList title="Web" projects={webProjects} />
+        <ProjectsList title="Mobile" projects={mobileProjects} />
+        <ProjectsList title="Jogos" projects={gameProjects} />
         <Curriculum technologies={technologies} />
       </Container>
     </Suspense>
