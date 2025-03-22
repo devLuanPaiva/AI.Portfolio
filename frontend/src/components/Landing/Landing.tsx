22c7aa22f28cf3794d51734d4fb60dd24fdbc560
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-22 08:26:07 -0300

    feat: change background image

--- frontend/src/components/Landing/Landing.tsx
+++ frontend/src/components/Landing/Landing.tsx
@@ -1,9 +1,9 @@
 export default function Landing(){
     return(
-        <section className="bg-[url(https://img.freepik.com/fotos-gratis/computador-portatil-oculos-rato-diario-caneta-e-maconha-planta-em-fundo-preto_23-2148042031.jpg?t=st=1742580859~exp=1742584459~hmac=cc6df38124b99b1970795fe4b5fe4c1ef55bb0e6ea842493cb5b1f1bf3c20294&w=1380)] bg-cover bg-center h-[600px] -mt-[100px] pt-[100px] z-0 flex flex-col  items-center justify-center ">
-            <h1 className="text-3xl sm:text-5xl font-bold text-center">
+        <section className="bg-[url(/bg.jpg)] bg-cover bg-center h-[600px] -mt-[100px] pt-[100px] z-0 flex flex-col  items-center justify-center ">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
                 Luan Alves de Paiva</h1>
-            <h2 className="text-zinc-500 ">Desenvolvedor Front-end</h2>
+            <h2 className="text-zinc-500  sm:text-xl md:text-2xl">Desenvolvedor Front-end</h2>
         </section>
     )
 }
