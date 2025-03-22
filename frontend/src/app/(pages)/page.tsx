3dfc8ef11848355b62a3876f87f6c616fdb14709
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-22 08:26:43 -0300

    feat: use suspense fallback and curriculum component

--- frontend/src/app/(pages)/page.tsx
+++ frontend/src/app/(pages)/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+import Curriculum from "@/components/document/Curriculum";
 import Landing from "@/components/Landing/Landing";
+import { useTechnologies } from "@/data/hooks/useTechnologies";
+import { Suspense } from "react";
 
 export default function Home() {
+  const { technologies, loading } = useTechnologies()
+  if (loading) return
+  
   return (
-    <Landing />
+    <Suspense fallback={<p>carregando...</p>} >
+      <Landing />
+      <Curriculum technologies={technologies} />
+    </Suspense>
   );
 }
