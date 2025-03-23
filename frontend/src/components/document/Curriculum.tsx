9615cde76171bd746816f2a157b130f90a3f4a05
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-23 16:34:19 -0300

    feat: create MinCV component

--- frontend/src/components/document/Curriculum.tsx
+++ frontend/src/components/document/Curriculum.tsx
@@ -1,13 +1,14 @@
 import { TechnologiesProps } from "@/data/interfaces";
 import TechnologiesWorked from "./TechnologiesWorked";
 import Experience from "./Experience";
+import MinCV from "./MinCV";
 
-export default function Curriculum(props: Readonly<TechnologiesProps> ){
-    return(
-       < section className="flex flex-col lg:flex-row gap-4 min-h-72 w-full">
-        <div className="flex-1"></div>
-           <Experience/>
-           <TechnologiesWorked technologies={props.technologies} />
-       </ section>
+export default function Curriculum(props: Readonly<TechnologiesProps>) {
+    return (
+        < section className="flex flex-col lg:flex-row gap-4 min-h-72 w-full">
+            <MinCV />
+            <Experience />
+            <TechnologiesWorked technologies={props.technologies} />
+        </ section>
     )
 }
