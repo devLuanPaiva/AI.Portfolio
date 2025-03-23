7ea4b9adf2fc48d596d66d28a42d6998fb741a9e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-23 17:09:56 -0300

    feat: update project page

--- frontend/src/app/(pages)/project/[id]/page.tsx
+++ frontend/src/app/(pages)/project/[id]/page.tsx
@@ -1,10 +1,21 @@
-export default async function PageProject(props: Readonly<{
-    params: Promise<{
-        id: string
-    }>
+'use client'
+import ProjectImagesList from "@/components/projects/ProjectImagesList";
+import Container from "@/components/template/Container";
+import { useProjectId } from "@/data/hooks/useProjects";
+
+export default function PageProject(props: Readonly<{
+    params: { id: string };
 }>) {
-    const { id } = await props.params
+    const { id } = props.params;
+    const { loading, project } = useProjectId(id);
+
+    if (loading) return <p>Carregando...</p>;
     return (
-        <p> {id} </p>
+        <section >
+            <Container classStyle="py-7 flex flex-col gap-10">
+                <h1 className="text-3xl font-bold"> {project?.name ?? 'não deu certo'} </h1>
+                <ProjectImagesList images={project?.images.slice(1)}/>
+            </Container>
+        </section>
     )
 }
