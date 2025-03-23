ec230a1656354d0dc6c1ce4c14e3f68e69bf5b05
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-23 15:00:01 -0300

    feat: implement infinity moving cards

--- frontend/src/components/projects/ProjectsList.tsx
+++ frontend/src/components/projects/ProjectsList.tsx
@@ -1,15 +1,63 @@
+'use client'
 import { ProjectsProps } from "@/data/interfaces";
 import ProjectItem from "./ProjectItem";
+import { useEffect, useRef, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function ProjectsList(props: Readonly<ProjectsProps>) {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const scrollerRef = useRef<HTMLUListElement>(null);
+
+    useEffect(() => {
+        addAnimation();
+    }, []);
+    const [start, setStart] = useState(false);
+    function addAnimation() {
+        if (containerRef.current && scrollerRef.current) {
+            const scrollerContent = Array.from(scrollerRef.current.children);
+
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                if (scrollerRef.current) {
+                    scrollerRef.current.appendChild(duplicatedItem);
+                }
+            });
+
+            getDirection();
+            getSpeed();
+            setStart(true);
+        }
+    }
+    const getDirection = () => {
+        if (containerRef.current) {
+            containerRef.current.style.setProperty(
+                "--animation-direction",
+                "forwards",
+            );
+        }
+    };
+    const getSpeed = () => {
+        if (containerRef.current) {
+            containerRef.current.style.setProperty("--animation-duration", "80s");
+        }
+    };
     return (
         <section>
             <h3 className="text-2xl font-bold text-white/70">{props.title} </h3>
-            <ul className="flex gap-4 flex-wrap">
-                {props.projects.map((project) => (
-                    <ProjectItem key={project.id} project={project} />
-                ))}
-            </ul>
+            <div ref={containerRef}
+                className={cn(
+                    "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]"
+                )}>
+                <ul ref={scrollerRef}
+                    className={cn(
+                        "flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
+                        start && "animate-scroll", "hover:[animation-play-state:paused]",
+                    )}>
+                    {props.projects.map((project) => (
+                        <ProjectItem key={project.id} project={project} />
+                    ))}
+                </ul>
+            </div>
         </section>
     )
 }
