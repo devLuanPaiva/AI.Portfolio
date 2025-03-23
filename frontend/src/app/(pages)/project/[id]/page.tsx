78c424cbc19ef69777844ffafa5ba07800ff6014
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-23 17:23:23 -0300

    fix: use useSearchParams hook

--- frontend/src/app/(pages)/project/[id]/page.tsx
+++ frontend/src/app/(pages)/project/[id]/page.tsx
@@ -1,21 +1,25 @@
-'use client'
+"use client";
 import ProjectImagesList from "@/components/projects/ProjectImagesList";
 import Container from "@/components/template/Container";
 import { useProjectId } from "@/data/hooks/useProjects";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function PageProject(props: Readonly<{
-    params: { id: string };
-}>) {
-    const { id } = props.params;
-    const { loading, project } = useProjectId(id);
+export default function PageProject() {
+    const searchParams = useSearchParams();
+    const id = searchParams.get('id');
+    const { loading, project } = useProjectId(id ?? '1');
 
     if (loading) return <p>Carregando...</p>;
+
     return (
-        <section >
-            <Container classStyle="py-7 flex flex-col gap-10">
-                <h1 className="text-3xl font-bold"> {project?.name ?? 'não deu certo'} </h1>
-                <ProjectImagesList images={project?.images.slice(1)}/>
-            </Container>
-        </section>
-    )
+        <Suspense fallback={<p>Carregando...</p>}>
+            <section>
+                <Container classStyle="py-7 flex flex-col gap-10">
+                    <h1 className="text-3xl font-bold">{project?.name ?? "Não deu certo"}</h1>
+                    <ProjectImagesList images={project?.images?.slice(1)} />
+                </Container>
+            </section>
+        </Suspense>
+    );
 }
