2763317eb9f57a287870418558efb36ee1e74338
Luan <devluanpaiva@gmail.com>
2025-03-24 11:33:22 -0300

    style: improve text and image size

--- frontend/src/components/projects/ProjectsList.tsx
+++ frontend/src/components/projects/ProjectsList.tsx
@@ -8,7 +8,9 @@ export default function ProjectsList(props: Readonly<ProjectsProps>) {
 	const { containerRef, scrollerRef, start } = useScrollerAnimation()
 	return (
 		<section>
-			<h3 className="text-2xl font-bold text-white/70">{props.title} </h3>
+			<h3 className="text-xl sm:text-2xl font-bold text-white/70">
+				{props.title}{" "}
+			</h3>
 			<div
 				ref={containerRef}
 				className={cn(
