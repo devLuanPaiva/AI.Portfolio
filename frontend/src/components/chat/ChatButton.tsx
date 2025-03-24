6cbd54c6aa33adc49d5297c168ca95b91a686cd5
Luan <devluanpaiva@gmail.com>
2025-03-24 08:36:54 -0300

    feat: call ChatWindow inside Poppover

--- frontend/src/components/chat/ChatButton.tsx
+++ frontend/src/components/chat/ChatButton.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
-import chatImage from '@/assets/chat.svg'
+import Image from "next/image"
+import chatImage from "@/assets/chat.svg"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import ChatWindow from "./ChatWindow"
 export default function ChatButton() {
-    return (
-        <button className="fixed bottom-5 right-5 cursor-pointer">
-            <Image src={chatImage} alt="ilustração de um chat" width={50} height={50} />
-        </button>
-    )
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<div className="fixed bottom-5 right-5 cursor-pointer">
+					<Image
+						src={chatImage}
+						alt="ilustração de um chat"
+						width={50}
+						height={50}
+					/>
+				</div>
+			</PopoverTrigger>
+			<PopoverContent
+				side="top"
+				align="end"
+				sideOffset={5}
+				className="w-[80%] sm:w-[500px] h-[70%]"
+				style={{ backgroundColor: "transparent", border: "none" }}
+			>
+				<ChatWindow />
+			</PopoverContent>
+		</Popover>
+	)
 }
