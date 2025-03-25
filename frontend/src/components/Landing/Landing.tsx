ccf8f2cb9c2938dcf4ccc4ab5b0b7b14c51b1a9b
Luan <devluanpaiva@gmail.com>
2025-03-25 13:03:28 -0300

    feat: change some texts

--- frontend/src/components/Landing/Landing.tsx
+++ frontend/src/components/Landing/Landing.tsx
@@ -1,15 +1,18 @@
-import { TechnologiesProps } from "@/data/interfaces";
-import Technologies from "../technologies/Technologies";
+import { TechnologiesProps } from "@/data/interfaces"
+import Technologies from "../technologies/Technologies"
 
 export default function Landing(props: Readonly<TechnologiesProps>) {
-    return (
-        <section className="bg-[url(/bg.jpg)] w-full bg-cover bg-center h-[600px] -mt-[100px] pt-[100px] z-0 flex flex-col  items-center justify-center gap-5 ">
-            <div className="flex flex-col items-center gap-1">
-                <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
-                    Luan Alves de Paiva</h1>
-                <h2 className="text-zinc-500  sm:text-xl md:text-2xl text-center">Desenvolvedor Front-end</h2>
-            </div>
-            <Technologies technologies={props.technologies} />
-        </section>
-    )
+	return (
+		<section className="bg-[url(/bg.jpg)] w-full bg-cover bg-center h-[600px] -mt-[100px] pt-[100px] z-0 flex flex-col  items-center justify-center gap-5 ">
+			<div className="flex flex-col items-center gap-1">
+				<h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
+					Luan Alves de Paiva
+				</h1>
+				<h2 className="text-zinc-500  sm:text-xl md:text-2xl text-center">
+					Desenvolvedor Front-end
+				</h2>
+			</div>
+			<Technologies technologies={props.technologies} />
+		</section>
+	)
 }
