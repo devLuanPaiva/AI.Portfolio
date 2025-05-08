931941ea08c9ada63b0a70a4eb4fe0046759087e
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-05-08 15:18:36 -0300

    feat: change all images path

--- frontend/src/data/constants/projects.const.ts
+++ frontend/src/data/constants/projects.const.ts
@@ -8,10 +8,12 @@ export const projects: Project[] = [
 		description:
 			"Um e-commerce de tecnologia que oferece uma ampla variedade de produtos, desde gadgets e acessórios até equipamentos de alta performance para gamers. Ideal para quem busca inovação e qualidade",
 		images: [
-			"https://github.com/portfolio-projetos-dev/gam3r-store/raw/main/.gitassets/1.jpg",
-			"https://github.com/portfolio-projetos-dev/gam3r-store/raw/main/.gitassets/2.jpg",
-			"https://github.com/portfolio-projetos-dev/gam3r-store/raw/main/.gitassets/3.jpg",
-			"https://github.com/portfolio-projetos-dev/gam3r-store/raw/main/.gitassets/4.jpg",
+			"https://ik.imagekit.io/p0mm3nebo/gameStore/image5.jp4?updatedAt=1746704038397",
+			"https://ik.imagekit.io/p0mm3nebo/gameStore/image5.jp4?updatedAt=1746704038397",
+			"https://ik.imagekit.io/p0mm3nebo/gameStore/image7.jpg?updatedAt=1746704038738",
+			"https://ik.imagekit.io/p0mm3nebo/gameStore/image2?updatedAt=1746629495638",
+			"https://ik.imagekit.io/p0mm3nebo/gameStore/image3?updatedAt=1746629508767",
+			"https://ik.imagekit.io/p0mm3nebo/gameStore/image4?updatedAt=1746629521891"
 		],
 		level: Level.EXPERT,
 		type: Type.WEB,
@@ -33,10 +35,10 @@ export const projects: Project[] = [
 		description:
 			"O Convite Digital é uma plataforma desenvolvida para facilitar a gestão e envio de convites digitais. Ele permite a criação, personalização e distribuição de convites para eventos de forma rápida e eficiente.",
 		images: [
-			"https://github.com/portfolio-projetos-dev/convit3-digital/raw/main/.gitassets/1.jpg",
-			"https://github.com/portfolio-projetos-dev/convit3-digital/raw/main/.gitassets/2.jpg",
-			"https://github.com/portfolio-projetos-dev/convit3-digital/raw/main/.gitassets/3.jpg",
-			"https://github.com/portfolio-projetos-dev/convit3-digital/raw/main/.gitassets/4.jpg",
+			"https://ik.imagekit.io/p0mm3nebo/evento/image1.jpg?updatedAt=1746727301093",
+			"https://ik.imagekit.io/p0mm3nebo/evento/image2.jpg?updatedAt=1746727301238",
+			"https://ik.imagekit.io/p0mm3nebo/evento/image3.jpg?updatedAt=1746727301963",
+			"https://ik.imagekit.io/p0mm3nebo/evento/image3.jpg?updatedAt=1746727301963",
 		],
 		level: Level.AVANCED,
 		type: Type.WEB,
@@ -58,8 +60,8 @@ export const projects: Project[] = [
 		description:
 			"Aplicação para agendamento e gerenciamento de horários em uma barbearia. Desenvolvido com NestJS no backend e Next.js com TypeScript no frontend.",
 		images: [
-			"https://github.com/portfolio-projetos-dev/barba-brutal/raw/main/.gitassets/1.jpg",
-			"https://github.com/portfolio-projetos-dev/barba-brutal/raw/main/.gitassets/2.jpg",
+			"https://ik.imagekit.io/p0mm3nebo/barbaBrutal/image1.png?updatedAt=1746727798240",
+			"https://ik.imagekit.io/p0mm3nebo/barbaBrutal/image2.png?updatedAt=1746727826238",
 		],
 		level: Level.AVANCED,
 		type: Type.MOBILE,
@@ -69,12 +71,12 @@ export const projects: Project[] = [
 			technologies[5],
 			technologies[9],
 			technologies[11],
-            technologies[12],
+			technologies[12],
 			technologies[14],
-            technologies[15],
-            technologies[16],
+			technologies[15],
+			technologies[16],
 			technologies[19],
 			technologies[20],
-        ],
+		],
 	},
 ]
